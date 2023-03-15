b705718f67c198e2acd6af8c4e14fe37ad4c954e
Atul Kumar <atulsaini649@gmail.com>
2023-03-15 22:08:59 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 export * from './components/Button';
-export * from './components/Input';
-export * from './components/Switch';
-export * from './components/Card';
-export * from './components/Table';
-export * from './components/Select';
-export * from './components/Alert';
-export * from './components/Tooltip';
+
